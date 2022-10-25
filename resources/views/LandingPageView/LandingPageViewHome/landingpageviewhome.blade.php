aed29e1b3aa10cb6330468aa038094fe3387a163
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-10-25 23:24:22 +0700

    home

--- resources/views/LandingPageView/LandingPageViewHome/landingpageviewhome.blade.php
+++ resources/views/LandingPageView/LandingPageViewHome/landingpageviewhome.blade.php
@@ -7,81 +7,56 @@
 {{-- Jumbptron Landing Page End --}}
 
 
-      <!-- About Start -->
-      <div class="container-xxl py-5">
-        <div class="container">
-          <div class="row g-5">
+<!-- About Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5">
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-              <div
-                class="position-relative overflow-hidden ps-5 pt-5 h-100"
-                style="min-height: 400px"
-              >
-                <img
-                  class="position-absolute w-100 h-100"
-                  src="{{ asset('LandingPageSource/img/about.jpg') }}"
-                  alt=""
-                  style="object-fit: cover"
-                />
-                <div
-                  class="position-absolute top-0 start-0 bg-white pe-3 pb-3"
-                  style="width: 200px; height: 200px"
-                >
-                  <div
-                    class="d-flex flex-column justify-content-center text-center bg-primary h-100 p-3"
-                  >
-                    <h1 class="text-white">25</h1>
-                    <h2 class="text-white">Years</h2>
-                    <h5 class="text-white mb-0">Experience</h5>
-                  </div>
+                <div class="position-relative overflow-hidden ps-5 pt-5 h-90 " style="min-height: 500px">
+                    <img class="position-absolute w-90 h-90 img-fluid" src="{{ asset('Images/Logos/logoldksyahid.png') }}" alt=""/>
                 </div>
-              </div>
+                <div class="border-top pt-3" style="margin-top: -10%">
+                    <div class="row">
+                        <p style="text-align: justify">
+                            LDK Syahid adalah salah satu Unit Kegiatan Mahasiswa (UKM) yang berada di Universitas Islam Negeri Syarif Hidayatullah Jakarta. UKM LDK Syahid ini bergerak
+                            dalam meningkatkan wawasan keilmuan & amal ibadah, pembinaan karakter & mental, serta pengasahan minat & bakat.
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-              <div class="h-100">
-                <div class="border-start border-5 border-primary ps-4 mb-5">
-                  <h6 class="text-body text-uppercase mb-2">About Us</h6>
-                  <h1 class="display-6 mb-0">
-                    Unique Solutions For Residentials & Industries!
-                  </h1>
+                <div class="h-100">
+                    <div class="border-start border-5 border-primary ps-4 mb-5">
+                        <h6 class="text-body text-uppercase mb-2">Tentang Kita</h6>
+                        <h1 class="display-6 mb-0">UKM LDK Syahid UIN Syarif Hidayatullah Jakarta</h1>
+                    </div>
+                    <h5>Visi</h5>
+                    <p style="text-align: justify">
+                        Menjadikan LDK Syahid sebagai tempat lahirnya insan dakwah yang memiliki high critical thinking, inkubator pelopor Amar Ma'ruf Nahi Munkar, dan mampu
+                        berkolaborasi serta berkontribusi dalam transformasi bangsa menuju Indonesia Madani
+                    </p>
+                    <h5>Misi</h5>
+                    <ol style="text-align: justify">
+                        <li>Membangungun kompetisi anggota yang rendah hati, open minded, objektif serta prestatif kontributif</li>
+                        <li>Meningkatkan jiwa critical thinking internal organisasi</li>
+                        <li>Pionir penebar kebermanfaatan baik skala nasional maupun internasional</li>
+                        <li>Berkolaborasi dengan berbagai pihak yang menunjang aktivitas dakwah baik secara internal maupun eksternal kampus</li>
+                        <li>Sebagai wadah konsolidasi para pemimpin menuju Indonesia Madani</li>
+                    </ol>
+                    {{-- <div class="border-top mt-3 pt-3">
+                        <div class="row g-4">
+                            <p style="text-align: justify">
+                                LDK Syahid adalah salah satu Unit Kegiatan Mahasiswa (UKM) yang berada di Universitas Islam Negeri Syarif Hidayatullah Jakarta. <br> LDK Syahid ini bergerak
+                                dalam meningkatkan wawasan keilmuan & amal ibadah, pembinaan karakter & mental, serta pengasahan minat & bakat.
+                            </p>
+                        </div>
+                    </div> --}}
                 </div>
-                <p>
-                  Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
-                  Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
-                  sed stet lorem sit clita duo justo magna dolore erat amet
-                </p>
-                <p class="mb-4">
-                  Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
-                  Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
-                  sed stet lorem sit clita duo justo magna dolore erat amet
-                </p>
-                <div class="border-top mt-4 pt-4">
-                  <div class="row g-4">
-                    <div class="col-sm-4 d-flex wow fadeIn" data-wow-delay="0.1s">
-                      <i
-                        class="fa fa-check fa-2x text-primary flex-shrink-0 me-3"
-                      ></i>
-                      <h6 class="mb-0">Ontime at services</h6>
-                    </div>
-                    <div class="col-sm-4 d-flex wow fadeIn" data-wow-delay="0.3s">
-                      <i
-                        class="fa fa-check fa-2x text-primary flex-shrink-0 me-3"
-                      ></i>
-                      <h6 class="mb-0">24/7 hours services</h6>
-                    </div>
-                    <div class="col-sm-4 d-flex wow fadeIn" data-wow-delay="0.5s">
-                      <i
-                        class="fa fa-check fa-2x text-primary flex-shrink-0 me-3"
-                      ></i>
-                      <h6 class="mb-0">Verified professionals</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-      <!-- About End -->
+    </div>
+</div>
+<!-- About End -->
 
       <!-- Facts Start -->
       <div class="container-fluid my-5 p-0">
