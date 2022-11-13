467ee478262dcdd549daa52d73703956c43b9276
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-11-13 21:34:10 +0700

    hadehh

--- resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
+++ resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
@@ -38,6 +38,15 @@
     <!-- Template Stylesheet -->
     <link href="{{ asset('AdminPageSource/css/style.css') }}" rel="stylesheet">
 
+    {{-- Datatable --}}
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" /> --}}
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
 </head>
 
