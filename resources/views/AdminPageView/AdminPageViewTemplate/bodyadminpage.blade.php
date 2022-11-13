f6d7910b3a4f90a265680bde74ecc2a29b6605aa
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-11-13 22:18:35 +0700

    aaa

--- resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
+++ resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> --}}
-    <title>Admin LDK Syahid</title>
+    <title>Admin LDK Syahid &#9679; {{ Auth::User()->name }}</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
