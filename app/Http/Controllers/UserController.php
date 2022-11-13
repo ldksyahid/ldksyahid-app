87c218753704d54069085cbb1503ccd90296ceb6
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-11-14 00:09:55 +0700

    gass

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Closure;
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Profile;
-use Illuminate\Foundation\Auth\ResetsPasswords;
-
+use \Yajra\Datatables\Datatables;
 
 class UserController extends Controller
 {
@@ -22,13 +18,31 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data = User::all();
-        return view('AdminPageView.AdminPageViewUsers.adminpageviewusers')->with([
-            'data' => $data,
-            "title" => "User"
-        ]);
+        if ($request->ajax()) {
+            $data = User::select('*');
+            return Datatables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+
+                        // $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+                        $btn = '
+                        <button class="btn btn-sm btn-primary" onClick="edit({{ $item->id }})"><i class="fa fa-edit"></i></button>
+                        <button class="btn btn-sm btn-primary" onClick="destroyuser({{ $item->id }})"><i class="fa fa-trash"></i></button>
+                        <button class="btn btn-sm btn-primary" onClick="preview({{ $item->id }})"><i class="fa fa-eye"></i></button>
+                        ';
+                        // $btn = '
+                        // <a class="btn btn-sm btn-primary" onClick="preview({{ $item->id }})"><i class="fa fa-eye"></i></a>
+                        // ';
+
+                        return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+
+        return view('AdminPageView.AdminPageViewUsers.adminpageviewusers')->with(["title" => "User"]);
     }
 
     public function read()
@@ -112,16 +126,6 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        // $passwordcr = Hash::make($request->password);
-        // $data = User::findOrFail($id);
-        // $passwordcr = Hash::make($request->password);
-        // $data['name'] = $request->name;
-        // $data['email'] = $request->email;
-        // $data['password'] = $passwordcr;
-        // $data['is_admin'] = $request->boolean('is_admin');
-        // User::save($data);
-        // toast('User has been edited !', 'success')->autoClose(1500)->width('350px');
-
         $passwordcr = Hash::make($request->password);
         $data = User::findOrFail($id);
         $data->name = $request->name;
@@ -153,3 +157,144 @@ class UserController extends Controller
         }
     }
 }
+
+// BACKUP START
+// class UserController extends Controller
+// {
+//     /**
+//      * Display a listing of the resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function index()
+//     {
+//         $data = User::all();
+//         return view('AdminPageView.AdminPageViewUsers.adminpageviewusers')->with([
+//             'data' => $data,
+//             "title" => "User"
+//         ]);
+//     }
+
+//     public function read()
+//     {
+//         $data = User::all();
+//         return view('AdminPageView.AdminPageViewUsers.adminpageviewusersread')->with([
+//             'data' => $data,
+//             "title" => "User"
+//         ]);
+//     }
+
+//     /**
+//      * Show the form for creating a new resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function create()
+//     {
+//         return view('AdminPageView.AdminPageViewUsers.adminpageviewuserscreate', ["title" => "User"]);
+//     }
+
+//     /**
+//      * Store a newly created resource in storage.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function store(Request $request)
+//     {
+//         $passwordcr = Hash::make($request->password);
+//         $data['name'] = $request->name;
+//         $data['email'] = $request->email;
+//         $data['password'] = $passwordcr;
+//         $data['is_admin'] = $request->boolean('is_admin');
+//         User::insert($data);
+//         Alert::success('Success', 'User created successfully !');
+//     }
+
+//     /**
+//      * Display the specified resource.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function show($id)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Show the form for editing the specified resource.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function edit($id)
+//     {
+//         $data = User::findOrFail($id);
+//         return view('AdminPageView.AdminPageViewUsers.adminpageviewusersedit')->with([
+//             'data' => $data,
+//             "title" => "User"
+//         ]);
+//     }
+
+//     public function preview($id)
+//     {
+//         $data = User::findOrFail($id);
+//         return view('AdminPageView.AdminPageViewUsers.adminpageviewuserspreview')->with([
+//             'data' => $data,
+//             "title" => "User"
+//         ]);
+//     }
+
+
+//     /**
+//      * Update the specified resource in storage.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function update(Request $request, $id)
+//     {
+//         // $passwordcr = Hash::make($request->password);
+//         // $data = User::findOrFail($id);
+//         // $passwordcr = Hash::make($request->password);
+//         // $data['name'] = $request->name;
+//         // $data['email'] = $request->email;
+//         // $data['password'] = $passwordcr;
+//         // $data['is_admin'] = $request->boolean('is_admin');
+//         // User::save($data);
+//         // toast('User has been edited !', 'success')->autoClose(1500)->width('350px');
+
+//         $passwordcr = Hash::make($request->password);
+//         $data = User::findOrFail($id);
+//         $data->name = $request->name;
+//         $data->email = $request->email;
+//         $data->password = $passwordcr;
+//         $data['is_admin'] = $request->boolean('is_admin');
+//         $data->save();
+//         toast('User has been edited !', 'success')->autoClose(1500)->width('350px');
+//     }
+
+//     /**
+//      * Remove the specified resource from storage.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function destroy($id)
+//     {
+//         $data = User::findOrFail($id);
+
+//         if ($data->id == 2) {
+//             Alert::error('Delete Failed', "^_^ Sory You Can't Delete Ucup ^_^");
+//             return redirect('/admin/user');
+//         } else {
+//             if ($data->profile == !null) {
+//                 File::delete($data->profile->profilepicture);
+//             }
+//             $data->delete();
+//         }
+//     }
+// }
+// BACKUP END
--- resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
+++ resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
@@ -38,6 +38,7 @@
     <link href="{{ asset('AdminPageSource/css/style.css') }}" rel="stylesheet">
 
     {{-- Datatable --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" /> --}}
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
@@ -89,8 +90,17 @@
     </div>
 
     <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+    {{-- DATATABLE --}}
+    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script> --}}
+    {{-- DATATABLE --}}
+
+    {{-- <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('AdminPageSource/lib/chart/chart.min.js') }}"></script>
