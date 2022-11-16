81c0813ff753ecbd5cf368e4b01450f194b96cac
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-11-17 00:08:27 +0700

    gass

--- resources/views/Print/printprofileview.blade.php
+++ resources/views/Print/printprofileview.blade.php
@@ -1,4 +1,55 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Print Layout &#9679; LDK Syahid</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <style>
+
+        </style>
+    </head>
+    {{-- <img src="{{ public_path(Auth::User()->profile->profilepicture) }}" alt=""> --}}
+    <body>
+        <div class="mx-3 my-5">
+            <div class="col-12">
+                <h3 class="m-0">
+                    <img src="{{ asset('Images/Logos/logoldksyahid.png') }}" width="55" height="55" alt="Logo LDK Syahid"> </i>UKM LDK Syahid
+                </h3>
+            </div>
+            <div class="col-12 my-3">
+                <div class="row">
+                    <div class="col-6">
+                        <h6>#KitaAdalahSaudara</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                        <a href="#" class="nav-link dropdown-toggle text-lowercase" data-bs-toggle="dropdown"><span class="mr-5">
+                            @if (Auth::User()->profile == null || Auth::User()->profile->profilepicture == null)
+                                <img class="rounded-circle" src="{{ Avatar::create(Auth::user()->name)->setFontFamily('Comic Sans MS')->setDimension(600)->setFontSize(325)->toBase64() }}" alt="" style="width: 27px; height: 27px;">
+                            @else
+                                <img class="rounded-circle" src="/{{Auth::User()->profile->profilepicture}}" alt="" style="width: 20px; height: 20px;">
+                            @endif
+                        </span>{{ Auth::user()->email }}</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 my-3">
+                <div class="col-6 text-center">
+                    <img src="../{{Auth::User()->profile->profilepicture}}" alt="" style="object-fit: cover"  width= "300px" height= "500px"/> <br> <i class="small">{{Auth::User()->profile->mottohidup}}</i>
+                </div>
+                <div class="col-6">
+
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <script>
+            window.print();
+        </script>
+    </body>
+</html>
+
+{{-- <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>
@@ -240,4 +291,4 @@
         @include('sweetalert::alert')
 
     </body>
-</html>
+</html> --}}
