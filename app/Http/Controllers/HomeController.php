72e442102e768938d08e06057cedfb7e4c35129a
gmayoeda <gmayoeda@gmail.com>
2022-11-25 13:43:43 +0700

    a

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,10 +33,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $postarticle= Article::orderBy('created_at','desc')->limit(3)->get();
+        $postarticle= Article::orderBy('dateevent','desc')->limit(3)->get();
         $postgallery= Gallery::orderBy('created_at','desc')->limit(1)->get();
-        $postevent= Event::orderBy('created_at','desc')->limit(3)->get();
-        $postnews= News::orderBy('created_at','desc')->limit(3)->get();
+        $postevent= Event::orderBy('dateevent','desc')->limit(3)->get();
+        $postnews= News::orderBy('datepublish','desc')->limit(3)->get();
         $postschedule= Schedule::orderBy('created_at','desc')->limit(1)->get();
         $postjumbotron= Jumbotron::orderBy('created_at','desc')->get();
         $posttestimony = Testimony::getAPITestimony()->orderBy('created_at','desc')->get();
--- resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
+++ resources/views/AdminPageView/AdminPageViewTemplate/bodyadminpage.blade.php
@@ -51,9 +51,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> --}}
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     {{-- END Datatable FROM https://www.itsolutionstuff.com/post/laravel-8-yajra-datatables-example-tutorialexample.html--}}
+
 
 </head>
 
