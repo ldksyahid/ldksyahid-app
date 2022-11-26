bccab68eac2bdff5466f9db59efaa9f62be2e520
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-11-26 22:44:35 +0700

    a

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,7 +48,7 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
+        if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password']), $input['remember'] ?? false))
         {
             if (auth()->user()->is_admin == 1) {
                 return redirect()->route('admin');
