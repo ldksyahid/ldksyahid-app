0cfaf1ded1f099e4f5854f93c2a8b318fc87e9db
My-Dios <55610692+My-Dios@users.noreply.github.com>
2022-11-26 22:44:35 +0700

    a

--- resources/views/LandingPageView/LandingPageViewArticle/landingpageviewarticle.blade.php
+++ resources/views/LandingPageView/LandingPageViewArticle/landingpageviewarticle.blade.php
@@ -22,14 +22,14 @@
             @forelse($postarticle as $key => $postarticle)
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="service-item bg-light overflow-hidden h-100">
-                    <img class="img-fluid" src="{{ asset($postarticle->poster) }}" alt="{{$postarticle->title}}" />
+                    <a href="/article/{{ $postarticle->id }}/show"><img class="img-fluid" src="{{ asset($postarticle->poster) }}" alt="{{$postarticle->title}}" /></a>
                     <div class="service-text position-relative text-center h-100 p-4">
                         <p class="text-end">{{ \Carbon\Carbon::parse( $postarticle->dateevent )->isoFormat('dddd') }}, {{ \Carbon\Carbon::parse( $postarticle->dateevent )->isoFormat('DD') }} {{ \Carbon\Carbon::parse( $postarticle->dateevent )->isoFormat('MMMM') }} {{ \Carbon\Carbon::parse( $postarticle->dateevent )->format('Y') }}</p>
                         <h6 class="text-body text-uppercase mb-2 text-start">{{ $postarticle->theme }}</h6>
-                        <h4 class="" style="text-align: left;">{{ $postarticle->title }}</h4>
-                        <p class="text-start mb-0">Penulis : {{ $postarticle->writer }}</p>
+                        <a href="/article/{{ $postarticle->id }}/show"><h4 class="" style="text-align: left;">{{ $postarticle->title }}</h4></a>
+                        <p class="text-start mb-0">Penulis :  {{ $postarticle->writer }}</p>
                         <p class="text-start mt-0">Editor : {{ $postarticle->editor }}</p>
-                        <div class="text-end">
+                        <div class="text-end align-bottom">
                             <a class="small" href="/article/{{ $postarticle->id }}/show">BACA SELENGKAPNYA<i class="fa fa-arrow-right ms-3"></i></a>
                         </div>
                     </div>
