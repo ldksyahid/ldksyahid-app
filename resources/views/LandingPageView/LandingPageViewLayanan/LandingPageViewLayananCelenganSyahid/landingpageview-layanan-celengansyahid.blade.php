c32e9aaa95da68bd30b58ae29fcb7aa18bf00bab
gmayoeda <gmayoeda@gmail.com>
2023-01-24 16:45:11 +0700

    a

--- resources/views/LandingPageView/LandingPageViewLayanan/LandingPageViewLayananCelenganSyahid/landingpageview-layanan-celengansyahid.blade.php
+++ resources/views/LandingPageView/LandingPageViewLayanan/LandingPageViewLayananCelenganSyahid/landingpageview-layanan-celengansyahid.blade.php
@@ -5,18 +5,22 @@
 @endsection
 
 @section('content')
-<div class="container-fluid p-0 mb-5 wow fadeIn hideIfMobile" data-wow-delay="0.2s">
-    <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active d-flex align-items-center" style="width: 100%; height: 560px; background-color:#000;">
-                <img class="w-100" src="{{ asset('Images/fixImage/dummy/excamp.png') }}"/>
-            </div>
-        </div>
-    </div>
+<div class="container-fluid p-0 mb-5 wow fadeIn" data-wow-delay="0.2s">
+  <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img class="w-100 " src="{{ asset('Images/fixImage/dummy/excamp.png') }}" alt="Image" />
+          </div>
+          <div class="carousel-item">
+              <img class="w-100 " src="{{ asset('Images/fixImage/dummy/excamp2.png') }}" alt="Image" />
+          </div>
+      </div>
+  </div>
 </div>
-<div class="container-xl" style="margin-top: -70px;">
+<div class="container-xl">
     <div class="row g-5">
-        <div class="col-lg-12 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        {{-- website --}}
+        <div class="col-lg-12 col-md-6 wow fadeInUp websiteResponsive" data-wow-delay="0.1s">
             <div class="border-start border-5 border-primary ps-4 mb-3">
                 <h6 class="text-body text-uppercase mb-2">Celengan Syahid</h6>
                 <h1 class="display-6 mb-0">
@@ -24,6 +28,19 @@
                 </h1>
             </div>
             <p class="mb-0" style="text-align: justify">
+                "Dan sembahlah Allah dan janganlah kamu mempersekutukan-Nya dengan sesuatu apa pun. Dan berbuat-baiklah kepada kedua orang tua, karib-kerabat, anak-anak yatim, orang-orang miskin, tetangga dekat dan tetangga jauh, teman sejawat, ibnu sabil dan hamba sahaya yang kamu miliki. Sungguh, Allah tidak menyukai orang yang sombong dan membanggakan diri,"
+                ● (QS. An-Nisa 4: Ayat 36)
+            </p>
+        </div>
+        {{-- mobile --}}
+        <div class="col-lg-12 col-md-6 wow fadeInUp mobileResponsive" data-wow-delay="0.1s">
+            <div class="border-start border-5 border-primary ps-4 mb-3">
+                <h6 class="text-body text-uppercase mb-2" style="font-size: 18px;">Celengan Syahid</h6>
+                <h1 class="display-6 mb-0" style="font-size: 24px;">
+                    Mari Bantu Mereka yang Membutuhkan
+                </h1>
+            </div>
+            <p class="mb-0" style="text-align: justify" style="font-size: 12px;">
                 "Dan sembahlah Allah dan janganlah kamu mempersekutukan-Nya dengan sesuatu apa pun. Dan berbuat-baiklah kepada kedua orang tua, karib-kerabat, anak-anak yatim, orang-orang miskin, tetangga dekat dan tetangga jauh, teman sejawat, ibnu sabil dan hamba sahaya yang kamu miliki. Sungguh, Allah tidak menyukai orang yang sombong dan membanggakan diri,"
                 ● (QS. An-Nisa 4: Ayat 36)
             </p>
@@ -80,16 +97,17 @@
         </div>
         <div class="col-lg-12 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
             <h6 class="text-body">Menampilkan 12 dari 62 <i>campaign</i></h6>
-            <div class="row g-4 mt-1">
+            {{-- website --}}
+            <div class="row g-4 mt-1 websiteResponsive">
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card mb-2">
                         <div style="height: 13em;">
-                            <a href=""><img src="{{ asset('Images/fixImage/dummy/excamp2.png') }}" alt="" class="card-img"></a>
+                            <a href=""><img src="{{ asset('Images/fixImage/dummy/excamp.png') }}" alt="" class="card-img w-100"></a>
                         </div>
                         <div class="mt-1 p-3">
                             <div class="badge" style="margin-left:-8px;"> <span>Sosial Dakwah</span> </div>
                             <div style="height: 5em;" class="d-flex flex-row align-items-center">
-                                <h5 class="text-body mb-0"><a href="">Sedekah Bangun Sekolah Terdampak Gempa Cianjur</a></h5>
+                                <h5 class="text-body mb-0"><a href="">Pojok Baca Pelosok Negeri : Membangun Bangsa Dengan Literasi</a></h5>
                             </div>
                             <div style="height: 2em;">
                                 <p style="font-size: 14px;">
@@ -135,54 +153,50 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            {{-- mobile --}}
+            <div class="row g-4 mt-1 mobileResponsive">
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card mb-2">
-                        <div style="height: 13em;">
-                            <a href=""><img src="{{ asset('Images/fixImage/dummy/excamp.png') }}" alt="" class="card-img"></a>
-                        </div>
-                        <div class="mt-1 p-3">
-                            <div class="badge" style="margin-left:-8px;"> <span>Sosial Dakwah</span> </div>
-                            <div style="height: 5em;" class="d-flex flex-row align-items-center">
-                                <h5 class="text-body mb-0"><a href="">Pojok Baca Pelosok Negeri : Membangun Bangsa Dengan Literasi</a></h5>
+                    <div class="card">
+                        <div class="row d-flex flex-row align-items-start">
+                            <div class="col" style="margin-right:-20px;">
+                                <div  class="p-3">
+                                    <a href=""><img src="{{ asset('Images/fixImage/dummy/excamp.png') }}" alt="" class="card-img w-100" style="border-radius: 5px;"></a>
+                                </div>
                             </div>
-                            <div style="height: 2em;">
-                                <p style="font-size: 14px;">
-                                    {!!  substr(strip_tags('Miris, literasi Indonesia posisi ke-62 dari 70 negara di dunia. Mari bangun bangsa dengan tingkatkan literasi melalui Pojok'), 0, 100) !!}
-                                </p>
-                            </div>
-                            <div class="mt-3">
-                                <hr>
-                                <div class="d-flex justify-content-between">
-                                    <div class="d-flex flex-row align-items-center" style="height: 2em;">
-                                        <img src="{{ asset('Images/Logos/logoldksyahid.png') }}" alt="logo" width="25" height="25">
-                                        <div class="ms-2 c-details">
-                                            <h6 style="font-size: 16px" class="mb-0 text-body"><a href="" target="_blank">UKM LDK Syahid</a></h6>
-                                        </div>
+                            <div class="col" style="margin-left:-20px;">
+                                <div class="p-3">
+                                    <div class="badge-mobile "> <span style="font-size:10px;"><b>Sosial Dakwah</b></span> </div>
+                                    <div style="line-height: 1.1em">
+                                        <h5 class=""><a href="" style="font-size: 12px ;">{!!  substr(strip_tags('Pojok Baca Pelosok Negeri : Membangun Bangsa Dengan Literasi'), 0, 25) !!}</a></h5>
+                                        <p style="font-size: 10px;">
+                                            {!!  substr(strip_tags('Miris, literasi Indonesia posisi ke-62 dari 70 negara di dunia. Mari bangun bangsa dengan tingkatkan literasi melalui Pojok'), 0, 100) !!}
+                                        </p>
                                     </div>
                                 </div>
-                                <div class="progress mt-3">
-                                    <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"><strong>50%</strong></div>
+                            </div>
+                        </div>
+                        <div class="col p-3" style="margin-top:-30px;">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <img src="{{ asset('Images/Logos/logoldksyahid.png') }}" alt="logo" width="20" height="20">
+                                    <div class="ms-2 c-details">
+                                        <h6 style="font-size: 10px" class="mb-0 text-body"><a href="" target="_blank">UKM LDK Syahid</a></h6>
+                                    </div>
                                 </div>
-                                <div class="mt-0">
-                                    <div class="row">
-                                        <div class="col text-start">
-                                            <p>
-                                                <span style="font-size: 12px;">Terkumpul</span>
-                                                <br>
-                                                <span style=" color:#00a79d;">
-                                                    <strong>Rp. 23.000,-</strong>
-                                                </span>
-                                            </p>
-                                        </div>
-                                        <div class="col text-end">
-                                            <p>
-                                                <span style="font-size: 12px;">Sisa Hari</span>
-                                                <br>
-                                                <span>
-                                                    <strong>365</strong>
-                                                </span>
-                                            </p>
-                                        </div>
+                            </div>
+                            <div class="progress  my-1" style="height: 13px;">
+                                <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"><strong style="font-size: 9px;">50%</strong></div>
+                            </div>
+                            <div class="mt-0">
+                                <div class="row">
+                                    <div class="col text-start" style="line-height: -50px;">
+                                        <p style="font-size: 9px;">Terkumpul</p>
+                                        <p style=" color:#00a79d; font-size:10px; margin-top:-18px;"><strong>Rp. 23.000,-</strong></p>
+                                    </div>
+                                    <div class="col text-end">
+                                        <p style="font-size: 9px;">Sisa Hari</p>
+                                        <p style="font-size:10px; margin-top:-18px;"><strong>365</strong></p>
                                     </div>
                                 </div>
                             </div>
