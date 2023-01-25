2c62a5ab1db63a57861ec8d85202c953f16cde78
gmayoeda <gmayoeda@gmail.com>
2023-01-25 09:34:37 +0700

    a

--- resources/views/LandingPageView/LandingPageViewLayanan/LandingPageViewLayananCelenganSyahid/landingpageview-layanan-celengansyahid-show.blade.php
+++ resources/views/LandingPageView/LandingPageViewLayanan/LandingPageViewLayananCelenganSyahid/landingpageview-layanan-celengansyahid-show.blade.php
@@ -29,23 +29,32 @@
                     </div>
                 </div>
                 <hr>
-                <div class="progress mt-3">
-                    <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"><strong>50%</strong></div>
+                <div class="mb-0">
+                    <div class="row d-flex flex-row align-items-end">
+                        <div class="col-lg-8 text-start">
+                            <h5 style=" color:#00a79d;"><strong>Rp. 23.000,-</strong></h5>
+                        </div>
+                        <div class="col-lg-4 text-end" style="margin-bottom: -20px;">
+                            <p style="font-size:12px;"><i class="fas fa-users fa-1x text-primary me-1 my-3"></i>5 Donatur</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="mt-0">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"><strong>75%</strong></div>
+                </div>
+                <div class="mb-0">
                     <div class="row d-flex flex-row align-items-center">
                         <div class="col-lg-8 text-start my-3">
                             <p style="font-size: 12px;">
                                 0.63% dari target Rp. 100.000.000,-
                             </p>
                         </div>
-                        <div class="col text-end my-3">
-                            <p style="font-size: 12px;" class="d-flex flex-row align-items-center">
-                                <i class="far fa-clock fa-1x text-primary me-1"></i>341 hari lagi
-                            </p>
+                        <div class="col-lg-4 text-end">
+                            <p style="font-size:12px;"><i class="far fa-clock fa-1x text-primary me-1 my-3"></i>341 hari lagi</p>
                         </div>
                     </div>
                 </div>
+                <hr>
             </div>
         </div>
     </div>
