b3f9c7eba6470a0e28b91e4de23e647f1c7787f3
gmayoeda <gmayoeda@gmail.com>
2023-01-30 11:47:57 +0700

    a

--- database/migrations/2023_01_16_235154_create_donations_table.php
+++ database/migrations/2023_01_16_235154_create_donations_table.php
@@ -10,13 +10,22 @@ class CreateDonationsTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */ 
+     */
     public function up()
     {
         Schema::create('donations', function (Blueprint $table) {
             $table->id();
-            $table->string('kategori');
-            $table->string('wilayah');
+            $table->string('jumlah_donasi')->nullable();
+            $table->string('nama_donatur')->nullable();
+            $table->string('email_donatur')->nullable();
+            $table->string('no_telp_donatur')->nullable();
+            $table->string('pesan_donatur')->nullable();
+            $table->string('captcha')->nullable();
+            $table->string('status')->nullable();
+            $table->string('metode_pembayaran')->nullable();
+            $table->string('nama_merchant')->nullable();
+            $table->string('biaya_admin')->nullable();
+            $table->string('kode_unik')->nullable();
             $table->timestamps();
         });
     }
