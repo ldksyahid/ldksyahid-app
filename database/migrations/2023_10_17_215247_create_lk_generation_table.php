4ff0dde5b086283b4a4672abf2fa486038f2f2f3
My-Dios <55610692+My-Dios@users.noreply.github.com>
2023-10-22 12:36:54 +0700

    [LDK-007] Add Migration Genefration Field

--- database/migrations/2023_10_17_215247_create_lk_generation_table.php
+++ database/migrations/2023_10_17_215247_create_lk_generation_table.php
@@ -19,9 +19,34 @@ class CreateLkGenerationTable extends Migration
             });
 
             DB::table('lk_generation')->insert([
-                ['year' => '2018', 'generationName' => 'Al-Muzammil'],
-                ['year' => '2019', 'generationName' => 'Al-Qomar'],
-                ['year' => '2020', 'generationName' => 'Al-Imran'],
+                ['year' => '1996', 'generationName' => '1996'],
+                ['year' => '1997', 'generationName' => '1997'],
+                ['year' => '1998', 'generationName' => '1998'],
+                ['year' => '1999', 'generationName' => '1999'],
+                ['year' => '2000', 'generationName' => 'Mentari'],
+                ['year' => '2001', 'generationName' => '2001'],
+                ['year' => '2002', 'generationName' => "Al-Ma'"],
+                ['year' => '2003', 'generationName' => "2003"],
+                ['year' => '2004', 'generationName' => "As-Syajaroh"],
+                ['year' => '2005', 'generationName' => "An-Nahl"],
+                ['year' => '2006', 'generationName' => "Al-Fath"],
+                ['year' => '2007', 'generationName' => "Ash-Shaf"],
+                ['year' => '2008', 'generationName' => "Al-Kahfi"],
+                ['year' => '2009', 'generationName' => "Al-Ashr"],
+                ['year' => '2010', 'generationName' => "An-Najm"],
+                ['year' => '2011', 'generationName' => "Al-Qolam"],
+                ['year' => '2012', 'generationName' => "Asy-Syams"],
+                ['year' => '2013', 'generationName' => "Al-Anfal"],
+                ['year' => '2014', 'generationName' => "An-Naml"],
+                ['year' => '2015', 'generationName' => "Az-Zukhruf"],
+                ['year' => '2016', 'generationName' => "Al-A'raaf"],
+                ['year' => '2017', 'generationName' => "Al-Isra"],
+                ['year' => '2018', 'generationName' => "Al-Muzzammil"],
+                ['year' => '2019', 'generationName' => "Al-Qomar"],
+                ['year' => '2020', 'generationName' => "Al-Furqan"],
+                ['year' => '2021', 'generationName' => "Ali-Imran"],
+                ['year' => '2022', 'generationName' => "Az-Zumar"],
+                ['year' => '2023', 'generationName' => "2023"],
             ]);
         }
     }
