a36e9e9ab87723213aa72b7463471a4863ccd6f6
My-Dios <55610692+My-Dios@users.noreply.github.com>
2023-12-26 21:59:05 +0700

    [LDK-008] ddition and adjustment of the Machine Learning Donation Celshahid Dashboard Feature

--- resources/views/admin-page/service/celengan-syahid/dashboard.blade.php
+++ resources/views/admin-page/service/celengan-syahid/dashboard.blade.php
@@ -5,14 +5,17 @@
     <div class="row g-4">
         <div class="col-12 col-lg-6">
             <div class="bg-light rounded h-100 p-4">
-                <h6 class="mb-4">Confusion Matrix Heatmap</h6>
-                <div id="heatmap"></div>
+                <div id="bar-plot"></div>
             </div>
         </div>
         <div class="col-12 col-lg-6">
             <div class="bg-light rounded h-100 p-4">
-                <h6 class="mb-4">Bar Plot</h6>
-                <div id="bar-plot"></div>
+                <div id="age-pie-chart"></div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-12">
+            <div class="bg-light rounded h-100 p-4">
+                <div id="bar-plot-2"></div>
             </div>
         </div>
     </div>
@@ -22,70 +25,189 @@
 @section('scripts')
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 <script>
-    function fetchConfusionMatrix() {
-        fetch('/svm-machine-output/confusion_matrix.json')
-            .then(response => response.json())
-            .then(data => {
-                const classLabels = ['4', '3', '2', '1', '0'];
-                const heatmapData = [{
-                    z: data,
-                    x: classLabels,
-                    y: classLabels,
-                    type: 'heatmap',
-                    colorscale: 'Viridis'
-                }];
-
-                const annotations = [];
-                for (let i = 0; i < classLabels.length; i++) {
-                    for (let j = 0; j < classLabels.length; j++) {
-                        annotations.push({
-                            x: classLabels[j],
-                            y: classLabels[i],
-                            text: data[i][j],
-                            showarrow: false,
-                        });
-                    }
-                }
-
-                const heatmapLayout = {
-                    title: 'SVM Predicted Dataset Donation Class',
-                    xaxis: { title: 'Predicted' },
-                    yaxis: { title: 'Actual' },
-                    annotations: annotations
-                };
-                Plotly.newPlot('heatmap', heatmapData, heatmapLayout);
-            })
-            .catch(error => console.error('Error fetching confusion matrix data:', error));
-    }
-
     function fetchBarData() {
-        fetch('/svm-machine-output/bar_plot_data.json')
+        fetch('/machine-learning/output/bar_plot_donation_class.json')
             .then(response => response.json())
             .then(data => {
-                const classLabels = ['0-25k', '26-50k', '51-100k', '101-250k', '>251k'];
                 const classCounts = data.class_counts;
 
+                const donationCategories = {
+                    'Low (0 - 25k)': classCounts[0],
+                    'Moderately Low (26k - 50k)': classCounts[1],
+                    'Moderate (51k - 100k)': classCounts[2],
+                    'Moderately High (101k - 250k)': classCounts[3],
+                    'High (> 251k)': classCounts[4]
+                };
+
+                // Mendapatkan nilai donasi dalam urutan yang diinginkan
+                const sortedCounts = Object.values(donationCategories);
+                const sortedCategories = Object.keys(donationCategories);
+
                 const barData = [{
-                    x: classLabels,
-                    y: classCounts,
+                    x: sortedCategories,
+                    y: sortedCounts,
                     type: 'bar',
                     marker: {
-                        color: 'green'
+                        color: ['rgba(173, 216, 230, 0.7)', 'rgba(144, 238, 144, 0.7)', 'rgba(255, 255, 0, 0.7)', 'rgba(255, 165, 0, 0.7)', 'rgba(255, 69, 132, 0.7)'],
+                        // Gunakan warna yang berbeda untuk setiap kategori donasi
                     },
-                    name: 'Bar Plot'
+                    name: 'Bar Plot',
+                    hoverinfo: 'text',
+                    text: sortedCounts.map(String),
+                    textposition: 'inside',
+                    hoverlabel: {
+                        bgcolor: 'white',
+                        bordercolor: 'black',
+                        font: {
+                            color: 'black'
+                        }
+                    }
                 }];
 
                 const barLayout = {
-                    title: 'SVM Predicted Donation Class',
-                    xaxis: { title: 'Donation Class' },
-                    yaxis: { title: 'Frequency' }
+                    title: 'Donation Class',
+                    xaxis: { title: 'Donation Category' },
+                    yaxis: { title: 'Donor Count' },
+                    margin: { t: 50, b: 120, l: 50, r: 60 }
                 };
 
                 Plotly.newPlot('bar-plot', barData, barLayout);
             })
             .catch(error => console.error('Error fetching bar plot data:', error));
     }
-    fetchConfusionMatrix();
     fetchBarData();
 </script>
+
+<script>
+    function fetchPieChartData() {
+        fetch('/machine-learning/output/pie_chart_age_category_counts.json')
+            .then(response => response.json())
+            .then(data => {
+                const ageLabels = Object.keys(data);
+                const ageCounts = Object.values(data);
+
+                const categoryOrder = [
+                    'Children (5-11 years old)',
+                    'Teenagers (12-25 years old)',
+                    'Adults (26-45 years old)',
+                    'Elderly (46-65 years old)',
+                    'Other Ages'
+                ];
+
+                const sortedAgeData = categoryOrder.reduce((acc, label) => {
+                    const index = ageLabels.indexOf(label);
+                    if (index !== -1) {
+                        acc.labels.push(label);
+                        acc.values.push(ageCounts[index]);
+                    }
+                    return acc;
+                }, { labels: [], values: [] });
+
+                const pieData = [{
+                    labels: sortedAgeData.labels,
+                    values: sortedAgeData.values,
+                    type: 'pie',
+                    marker: {
+                        colors: ['rgba(173, 216, 230, 0.7)', 'rgba(144, 238, 144, 0.7)', 'rgba(255, 255, 0, 0.7)', 'rgba(255, 165, 0, 0.7)', 'rgba(255, 69, 132, 0.7)']
+                        // Gunakan warna yang berbeda untuk setiap kategori usia
+                    },
+                    hole: 0.4 // Bagian tengah yang kosong untuk efek donat
+                }];
+
+                const pieLayout = {
+                    title: 'Percentage of Donors Based on Age Category',
+                    legend: {
+                        x: 1,
+                        y: 0.5,
+                        traceorder: 'normal',
+                        font: {
+                            family: 'Arial',
+                            size: 12,
+                            color: '#000'
+                        },
+                        categoryorder: 'array',
+                        categoryarray: categoryOrder
+                    },
+                };
+
+                Plotly.newPlot('age-pie-chart', pieData, pieLayout);
+            })
+            .catch(error => console.error('Error fetching pie chart data:', error));
+    }
+    fetchPieChartData();
+</script>
+
+<script>
+    function fetchBarData2() {
+        fetch('/machine-learning/output/bar_plot_count_age_donation.json')
+            .then(response => response.json())
+            .then(data => {
+                const ageCategories = data.age_category;
+                const donationCategories = data.donation_category;
+                const donorCounts = data.donor_count;
+
+                const colors = ['rgba(173, 216, 230, 0.7)', 'rgba(144, 238, 144, 0.7)', 'rgba(255, 255, 0, 0.7)', 'rgba(255, 165, 0, 0.7)', 'rgba(255, 69, 132, 0.7)'];
+
+                const dataDict = {};
+                const order = ['Low (0 - 25k)', 'Moderately Low (26k - 50k)', 'Moderate (51k - 100k)', 'Moderately High (101k - 250k)', 'High (> 251k)'];
+                const categoryOrder = [
+                    'Children (5-11 years old)',
+                    'Teenagers (12-25 years old)',
+                    'Adults (26-45 years old)',
+                    'Elderly (46-65 years old)',
+                    'Other Ages'
+                ];
+
+                for (let i = 0; i < ageCategories.length; i++) {
+                    const ageCategory = ageCategories[i];
+                    const donationCategory = donationCategories[i];
+                    const donorCount = donorCounts[i];
+
+                    if (!dataDict[donationCategory]) {
+                        dataDict[donationCategory] = {
+                            x: [],
+                            y: [],
+                            type: 'bar',
+                            name: donationCategory,
+                            textposition: 'auto',
+                            text: []
+                        };
+                    }
+
+                    dataDict[donationCategory].x.push(ageCategory);
+                    dataDict[donationCategory].y.push(donorCount);
+                    dataDict[donationCategory].text.push(donorCount);
+                }
+
+                const orderedBarData = order.map(category => dataDict[category]);
+                orderedBarData.forEach((bar, index) => {
+                    bar.marker = { color: colors[index % colors.length] }; // Menggunakan palet warna yang sama
+                    bar.hoverinfo = 'name';
+                });
+
+                const barLayout = {
+                    barmode: 'group',
+                    title: 'Donor Counts by Age and Donation Category',
+                    xaxis: {
+                        title: 'Age Category',
+                        categoryorder: 'array',
+                        categoryarray: categoryOrder,
+                        automargin: true
+                    },
+                    yaxis: { title: 'Donor Count' },
+                    legend: { title: { text: 'Donation Category' } },
+                    margin: { t: 50, b: 100, l: 50, r: 50 },
+                    transition: {
+                        duration: 500,
+                        easing: 'cubic-in-out'
+                    }
+                };
+
+                Plotly.newPlot('bar-plot-2', orderedBarData, barLayout);
+            })
+            .catch(error => console.error('Error fetching bar plot 2 data:', error));
+    }
+    fetchBarData2();
+</script>
+
 @endsection
