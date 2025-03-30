a671ed986ceddd9281d8e61efc0e2672fb578479
Yusuf Wijaya <yusufwijaya3@gmail.com>
2025-03-31 00:24:32 +0700

    [ENHANCEMENT] Remove Visitor count in dashboar d index admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,13 +21,6 @@ class DashboardController extends Controller
         $eventCount = Event::count();
         $shortLinkCount = ShortURL::count();
 
-        // Hitung jumlah pengunjung unik
-        if (!Session::has('visitor_counted')) {
-            Session::put('visitor_counted', true);
-            Session::increment('visitor_count');
-        }
-        $visitorCount = Session::get('visitor_count', 0);
-
         // Ambil data waktu solat dari API Kemenag
         $cityId = 1301; // ID kota untuk Jakarta Pusat (ganti sesuai lokasi)
         $date = date('Y-m-d'); // Tanggal hari ini
@@ -42,7 +35,6 @@ class DashboardController extends Controller
             'articleCount' => $articleCount,
             'eventCount' => $eventCount,
             'shortLinkCount' => $shortLinkCount,
-            'visitorCount' => $visitorCount,
             'prayerTimes' => $prayerTimes,
         ]);
     }
