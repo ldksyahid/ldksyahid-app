ef505f0e0d1ef3cb6b274f9a4c54e96b3be044aa
Yusuf Wijaya <yusufwijaya3@gmail.com>
2025-05-30 22:08:40 +0700

    [ENHANCEMENT] change email official

--- resources/views/landing-page/template/body.blade.php
+++ resources/views/landing-page/template/body.blade.php
@@ -45,7 +45,7 @@
     </head>
 
     <body id="body-lp">
-
+        @yield('styles')
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-grow text-primary" role="status"></div>
         </div>
