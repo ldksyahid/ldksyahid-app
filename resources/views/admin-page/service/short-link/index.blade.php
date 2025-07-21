ec50658ed79c3ae30eaeaaf045f5f71666b203d3
Yusuf Wijaya <yusufwijaya3@gmail.com>
2025-07-22 00:57:36 +0700

    remove bundle

--- resources/views/admin-page/service/short-link/index.blade.php
+++ resources/views/admin-page/service/short-link/index.blade.php
@@ -467,7 +467,6 @@
 
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     $(document).ready(function() {
         const baseUrl = '{{ url('/') }}';
