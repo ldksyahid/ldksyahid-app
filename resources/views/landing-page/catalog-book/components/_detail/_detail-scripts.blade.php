3600ea3dc7225ede2870cbd97ff09b124511ad4b
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2025-10-27 17:23:21 +0700

    feat remove detail catalog book

--- resources/views/landing-page/catalog-book/components/_detail/_detail-scripts.blade.php
+++ resources/views/landing-page/catalog-book/components/_detail/_detail-scripts.blade.php
@@ -1,12 +1,3 @@
 <script>
-$(document).ready(function() {
-    $('.card').hover(
-        function() {
-            $(this).find('.card-header').css('background', 'linear-gradient(135deg, #006D6D 0%, #008F8F 100%)');
-        },
-        function() {
-            $(this).find('.card-header').css('background', 'linear-gradient(135deg, #008F8F 0%, #006D6D 100%)');
-        }
-    );
-});
+
 </script>
