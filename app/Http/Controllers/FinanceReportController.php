f075a8226ddddde8eb01435e4f5859778fdd15b1
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2025-12-18 21:12:55 +0700

    feat: add controller and model finance report

---
+++ app/Http/Controllers/FinanceReportController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MsCatalogBook;
+use App\Models\LkBookCategory;
+use App\Models\LkLanguage;
+use App\Models\LkAuthorType;
+use App\Models\LkAvailabilityType;
+use Illuminate\Http\Request;
+
+class FinanceReportController extends Controller
+{
+    /* =========================================================================
+       SECTION B — ADMIN AREA (with RESTful routing)
+       ========================================================================= */
+    public function indexAdmin(Request $request)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function showAdmin(MsCatalogBook $book)
+    {
+
+    }
+
+    public function edit(MsCatalogBook $book)
+    {
+
+    }
+
+    public function update(Request $request, MsCatalogBook $book)
+    {
+
+    }
+
+    public function destroy(MsCatalogBook $book)
+    {
+
+    }
+
+    public function bulkDelete(Request $request)
+    {
+
+    }
+}
