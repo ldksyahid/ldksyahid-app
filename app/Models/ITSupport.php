ed5afc2609e6ee6d37caf76766f0498ee422287c
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-01-29 22:03:46 +0700

    feat: implement merge component for it support menu admin

--- app/Models/ITSupport.php
+++ app/Models/ITSupport.php
@@ -4,10 +4,254 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\GoogleDrive;
+use Illuminate\Http\Request;
 
 class ITSupport extends Model
 {
     use HasFactory;
-    protected $guarded =[];
-}
+    protected $guarded = [];
 
+    public const PATH_ITSUPPORT_GDRIVE_ID = '1gE3j9fXZIicfqeFqTYuc5JFpJe-FbSBs';
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Allowed sort columns
+     */
+    protected static array $allowedSorts = [
+        'name',
+        'forkat',
+        'position',
+        'created_at',
+    ];
+
+    /**
+     * Get table configuration for admin index table component
+     */
+    public static function getTableConfig(): array
+    {
+        return [
+            'idKey' => 'id',
+            'emptyMessage' => 'No IT Support found',
+            'emptyIcon' => 'fa-headset',
+            'colspan' => 8,
+            'columns' => [
+                [
+                    'key' => 'name',
+                    'type' => 'text',
+                    'class' => 'text-start',
+                ],
+                [
+                    'key' => 'forkat',
+                    'type' => 'text',
+                    'class' => 'text-center',
+                ],
+                [
+                    'key' => 'position',
+                    'type' => 'text',
+                    'class' => 'text-center',
+                ],
+                [
+                    'key' => 'linkInstagram',
+                    'type' => 'link',
+                    'class' => 'text-center',
+                    'fallback' => '-',
+                ],
+                [
+                    'key' => 'linkLinkedin',
+                    'type' => 'link',
+                    'class' => 'text-center',
+                    'fallback' => '-',
+                ],
+            ],
+            'actions' => [
+                'view' => [
+                    'enabled' => true,
+                    'route' => 'admin.about.itsupport.preview',
+                    'routeKey' => 'id',
+                ],
+                'edit' => [
+                    'enabled' => true,
+                    'type' => 'link',
+                    'route' => 'admin.about.itsupport.edit',
+                    'routeKey' => 'id',
+                ],
+                'delete' => [
+                    'enabled' => true,
+                    'btnClass' => 'delete-itsupport-btn',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Get unique forkat options for select2 filter
+     */
+    public static function getForkatOptions(): array
+    {
+        return self::select('forkat')
+            ->distinct()
+            ->orderBy('forkat')
+            ->pluck('forkat', 'forkat')
+            ->filter()
+            ->toArray();
+    }
+
+    /**
+     * Get unique position options for select2 filter
+     */
+    public static function getPositionOptions(): array
+    {
+        return self::select('position')
+            ->distinct()
+            ->orderBy('position')
+            ->pluck('position', 'position')
+            ->filter()
+            ->toArray();
+    }
+
+    /**
+     * Search and filter IT Supports for admin panel with pagination
+     */
+    public static function searchAdminITSupports(Request $request)
+    {
+        $query = self::query();
+
+        // Search by name
+        if ($request->filled('name')) {
+            $query->where('name', 'like', '%' . $request->name . '%');
+        }
+
+        // Filter by forkat
+        if ($request->filled('forkat')) {
+            $query->where('forkat', $request->forkat);
+        }
+
+        // Filter by position
+        if ($request->filled('position')) {
+            $query->where('position', $request->position);
+        }
+
+        // Filter by created date range
+        if ($request->filled('created_at_start') && $request->filled('created_at_end')) {
+            $startDate = \Carbon\Carbon::createFromFormat('d-m-Y', $request->created_at_start)->startOfDay();
+            $endDate = \Carbon\Carbon::createFromFormat('d-m-Y', $request->created_at_end)->endOfDay();
+            $query->whereBetween('created_at', [$startDate, $endDate]);
+        }
+
+        // Sorting
+        $sortBy = $request->input('sort_by', 'created_at');
+        $sortOrder = $request->input('sort_order', 'desc');
+
+        if (!in_array($sortBy, static::$allowedSorts)) {
+            $sortBy = 'created_at';
+        }
+
+        $query->orderBy($sortBy, $sortOrder);
+
+        return $query->paginate(15)->appends($request->query());
+    }
+
+    /**
+     * Save new IT Support
+     */
+    public static function saveModel(Request $request): self
+    {
+        $gdriveService = new GoogleDrive(self::PATH_ITSUPPORT_GDRIVE_ID);
+
+        $fileName = time() . '_it-supports_' . $request->file('photoProfile')->getClientOriginalName();
+        $filePath = self::PATH_ITSUPPORT_GDRIVE_ID . '/' . $fileName;
+
+        $uploadResult = $gdriveService->uploadImage($request->file('photoProfile'), $fileName, $filePath);
+
+        return self::create([
+            'name' => $request->name,
+            'forkat' => $request->forkat,
+            'position' => $request->position,
+            'linkInstagram' => $request->linkInstagram,
+            'linkLinkedin' => $request->linkLinkedin,
+            'gdrive_id' => $uploadResult['gdriveID'],
+            'photoProfile' => $uploadResult['fileName'],
+        ]);
+    }
+
+    /**
+     * Update existing IT Support
+     */
+    public function updateModel(Request $request): self
+    {
+        if ($request->hasFile('photoProfile')) {
+            $gdriveService = new GoogleDrive(self::PATH_ITSUPPORT_GDRIVE_ID);
+
+            $fileName = time() . '_it-supports_' . $request->file('photoProfile')->getClientOriginalName();
+            $filePath = self::PATH_ITSUPPORT_GDRIVE_ID . '/' . $fileName;
+
+            $uploadResult = $gdriveService->uploadImage($request->file('photoProfile'), $fileName, $filePath);
+
+            if ($this->gdrive_id) {
+                $gdriveService->deleteImage($this->gdrive_id);
+            }
+
+            $this->update([
+                'photoProfile' => $uploadResult['fileName'],
+                'gdrive_id' => $uploadResult['gdriveID'],
+            ]);
+        }
+
+        $this->update([
+            'name' => $request->name,
+            'forkat' => $request->forkat,
+            'position' => $request->position,
+            'linkInstagram' => $request->linkInstagram,
+            'linkLinkedin' => $request->linkLinkedin,
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * Delete IT Support and its image from Google Drive
+     */
+    public function deleteModel(): bool
+    {
+        $gdriveService = new GoogleDrive(self::PATH_ITSUPPORT_GDRIVE_ID);
+
+        if ($this->gdrive_id) {
+            $gdriveService->deleteImage($this->gdrive_id);
+        }
+
+        return $this->delete();
+    }
+
+    /**
+     * Bulk delete IT Supports
+     */
+    public static function bulkDeleteModel(array $ids): int
+    {
+        $items = self::whereIn('id', $ids)->get();
+        $gdriveService = new GoogleDrive(self::PATH_ITSUPPORT_GDRIVE_ID);
+
+        foreach ($items as $item) {
+            if ($item->gdrive_id) {
+                $gdriveService->deleteImage($item->gdrive_id);
+            }
+        }
+
+        return self::whereIn('id', $ids)->delete();
+    }
+
+    /**
+     * Get photo profile URL from Google Drive
+     */
+    public function getPhotoProfileUrl(): ?string
+    {
+        if ($this->gdrive_id) {
+            return "https://lh3.googleusercontent.com/d/{$this->gdrive_id}";
+        }
+        return null;
+    }
+}
--- resources/views/admin-page/about/it-support/view.blade.php
+++ resources/views/admin-page/about/it-support/view.blade.php
@@ -1,48 +1,16 @@
 @extends('admin-page.template.body')
 
+@section('styles')
+    @include('admin-page.about.it-support.components._form._form-styles')
+@endsection
+
 @section('content')
-<!-- Form Start -->
-<div class="container-fluid pt-4 px-4">
-    <div class="row g-4">
-        <div class="col-12">
-            <div class="bg-light rounded h-100 p-4">
-                <h5 class="mb-4">Preview IT Support</h5>
-                <form role="form" action='#' method='post' enctype="multipart/form-data">
-                    <div class="row">
-                        <div class="mb-3 col-12 col-lg-4">
-                            <label for="inputName" class="form-label required">Name</label>
-                            <input type="text" class="form-control" id="inputName" name='name' value="{{old('name', $postitsupport->name)}}" disabled>
-                        </div>
-                        <div class="mb-3 col-12 col-lg-4">
-                            <label for="inputForkat" class="form-label required">Forkat</label>
-                            <input type="text" class="form-control" id="inputForkat" name='forkat' value="{{old('forkat', $postitsupport->forkat)}}" disabled>
-                        </div>
-                        <div class="mb-3 col-12 col-lg-4">
-                            <label for="inputPosition" class="form-label required">Position</label>
-                            <input type="text" class="form-control" id="inputPosition" name='position' value="{{old('position', $postitsupport->position)}}" disabled>
-                        </div>
-                        <div class="mb-3 col-12 col-lg-4">
-                            <label for="photoProfile" class="form-label required">Photo Profile <span class="small">(300 x 350 Pixel)</span></label>
-                            <br>
-                            <img id="frame" src="https://lh3.googleusercontent.com/d/{{ $postitsupport->gdrive_id }}" width="200px" height="250px"  class="rounded mb-3 border"/>
-                            <input class="form-control" type="file" id="photoProfile" name = 'photoProfile' accept="image/png, image/jpeg, image/jpg, image/JPG, image/PNG, image/webp" disabled>
-                        </div>
-                        <div class="mb-3 col-12 col-lg-4">
-                            <label for="inputLinkInstagram" class="form-label required">Instagram Link</label>
-                            <input type="text" class="form-control" id="inputLinkInstagram" name='linkInstagram' value="{{old('linkInstagram', $postitsupport->linkInstagram)}}" disabled>
-                        </div>
-                        <div class="mb-3 col-12 col-lg-4">
-                            <label for="inputLinkInstagram" class="form-label required">Linkedin Link</label>
-                            <input type="text" class="form-control" id="inputLinkInstagram" name='linkLinkedin' value="{{old('linkLinkedin', $postitsupport->linkLinkedin)}}" disabled>
-                        </div>
-                        <div class="mb-3 col-12">
-                            <a class="btn btn-primary" href="/admin/about/itsupport"><i class="fa fa-arrow-left"></i> Back</a>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Form End -->
+    @include('admin-page.about.it-support.components._form._form', [
+        'operation' => 'view',
+        'itsupport' => $itsupport
+    ])
+@endsection
+
+@section('scripts')
+    @include('admin-page.about.it-support.components._form._form-scripts')
 @endsection
