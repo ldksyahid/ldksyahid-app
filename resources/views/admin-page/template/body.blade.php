978b8ccf13b9f9a43058939d73656b2b0a378221
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-02-01 10:06:12 +0700

    fixing .dark-mode-checkbox:checked + .dark-mode-toggle:hover .toggle-thumb

--- resources/views/admin-page/template/body.blade.php
+++ resources/views/admin-page/template/body.blade.php
@@ -264,7 +264,11 @@
             }
 
             .dark-mode-checkbox:checked + .dark-mode-toggle .toggle-thumb {
-                transform: translateX(25px);
+                transform: translateX(23px);
+            }
+
+            .dark-mode-checkbox:checked + .dark-mode-toggle:hover .toggle-thumb {
+                transform: translateX(23px) scale(1.1);
             }
 
             .sun-icon svg, .moon-icon svg {
