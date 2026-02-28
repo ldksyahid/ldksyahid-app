70e55f885a5d2276857aad55066575f077bf7061
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-02-28 22:44:35 +0700

    feat: add ms setting

---
+++ database/migrations/2026_02_28_223922_create_ms_setting_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Constants\SettingKey\Key1;
+use App\Constants\SettingKey\Key2;
+use App\Models\MsSetting;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMsSettingTable extends Migration
+{
+    public function up(): void
+    {
+        if (!Schema::hasTable(MsSetting::getTableName())) {
+            Schema::create(MsSetting::getTableName(), function (Blueprint $table) {
+                $table->bigIncrements('settingID');
+
+                $table->string('key1', 1000)->index();
+                $table->string('key2', 1000)->index();
+                $table->string('value1', 1000)->nullable();
+                $table->string('value2', 1000)->nullable();
+
+                $table->unique(['key1', 'key2']);
+            });
+
+            // Insert default row
+            DB::table(MsSetting::getTableName())->insert([
+                'key1'   => Key1::LAYANAN,
+                'key2'   => Key2::CpShortlink,
+                'value1' => '+62895394755672',
+                'value2' => null,
+            ]);
+        }
+    }
+
+    public function down(): void
+    {
+        if (Schema::hasTable(MsSetting::getTableName())) {
+            Schema::dropIfExists(MsSetting::getTableName());
+        }
+    }
+}
