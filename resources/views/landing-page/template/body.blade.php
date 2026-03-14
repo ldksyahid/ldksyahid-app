0fa9fc70cdafa79ad6cea850f6430b59f0476523
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-03-14 21:13:37 +0700

    (refactor-revamp/welcome-popup) add welcome pop up

--- resources/views/landing-page/template/body.blade.php
+++ resources/views/landing-page/template/body.blade.php
@@ -216,6 +216,9 @@
              modal-backdrop at z-index:1040. Modals pushed here live at body level and work correctly.) --}}
         @stack('modals')
 
+        {{-- Welcome Revamp Popup (tampil sekali, ditandai di localStorage) --}}
+        @include('components.welcome-revamp-popup.index')
+
         {{-- Back to Top Button --}}
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
             <i class="bi bi-arrow-up"></i>
