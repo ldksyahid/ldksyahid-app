9ea4e2ba5a162e56e66e9830cb6952248ef7c2b9
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-03-24 10:52:12 +0700

    (security/project) add security medium

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,9 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\ForceHttps::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\SecurityHeaders::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceHttps
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Hanya aktif di production dan jika belum HTTPS
+        if (app()->environment('production') && !$request->secure()) {
+            return redirect()->secure($request->getRequestUri(), 301);
+        }
+
+        return $next($request);
+    }
+}
