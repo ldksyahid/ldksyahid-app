faecd8ab3ec84044e904ececd6df8de178c9c0c8
Farisifakhri <mu.fakhrialfarisi@gmail.com>
2026-04-22 16:16:13 +0700

    feat: update zakat calculator dan routing

--- routes/web.php
+++ routes/web.php
@@ -76,10 +76,17 @@ Route::get('/kalkulatorkestari', function () {
     return view('landing-page.service.proker-counter.index', ["title" => "Layanan"]);
 });
 
+// Route LandingPage Layanan => Kalkulator Zakat
+Route::get('/service/zakat-calculator', function () {
+    return view('landing-page.service.zakat-calculator.index', ["title" => "Kalkulator Zakat"]);
+})->name('zakat-calculator');
+
 // Route LandingPage Layanan
 Route::get('/service', function () {
     return view('landing-page.service.index', ["title" => "Layanan"]);
 });
+
+
 
 // Route LandingPage Artikel
 Route::get('/articles', [ArticleController::class, 'index'])->name('article.index');
