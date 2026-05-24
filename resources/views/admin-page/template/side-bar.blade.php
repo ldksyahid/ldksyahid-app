fa72b1021c8a2845bf01716cecb8598b9053f53e
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-05-24 21:25:01 +0700

    (feat/auth) enhance google avatar

--- resources/views/admin-page/template/side-bar.blade.php
+++ resources/views/admin-page/template/side-bar.blade.php
@@ -22,10 +22,12 @@
         </a>
         <div class="d-flex align-items-center ms-4 mb-4">
             <div class="position-relative">
-                @if (Auth::User()->profile == null || Auth::User()->profile->profilepicture == null)
-                    <img class="rounded-circle" src="{{ Avatar::create(Auth::user()->name)->setFontFamily('Comic Sans MS')->setDimension(600)->setFontSize(325)->toBase64() }}" alt="" style="width: 40px; height: 40px;">
-                @else
+                @if (Auth::User()->profile != null && Auth::User()->profile->profilepicture != null)
                     <img class="rounded-circle" src="https://lh3.googleusercontent.com/d/{{Auth::User()->profile->gdrive_id}}" alt="{{Auth::User()->profile->namapanggilan}}" style="width: 40px; height: 40px;">
+                @elseif (Auth::User()->profile != null && Auth::User()->profile->googleAvatar)
+                    <img class="rounded-circle" src="{{ Auth::User()->profile->googleAvatar }}" alt="{{ Auth::user()->name }}" style="width: 40px; height: 40px;">
+                @else
+                    <img class="rounded-circle" src="{{ Avatar::create(Auth::user()->name)->setFontFamily('Comic Sans MS')->setDimension(600)->setFontSize(325)->toBase64() }}" alt="" style="width: 40px; height: 40px;">
                 @endif
                 <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
             </div>
