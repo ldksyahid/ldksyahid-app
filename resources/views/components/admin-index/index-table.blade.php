72a3d2bea6d6572bd05b6ed766246058410908e6
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2026-05-31 20:23:40 +0700

    (enhance/user-admin) add flex to show google icon

--- resources/views/components/admin-index/index-table.blade.php
+++ resources/views/components/admin-index/index-table.blade.php
@@ -197,10 +197,10 @@
                         @break
 
                     @case('email-with-google')
-                        <div class="d-flex align-items-center gap-1">
-                            <span>{{ $value ?: $fallback }}</span>
+                        <div class="d-flex align-items-center gap-1" style="min-width:0;">
+                            <span style="overflow:hidden;text-overflow:ellipsis;white-space:nowrap;min-width:0;flex:1;" title="{{ $value ?: $fallback }}">{{ $value ?: $fallback }}</span>
                             @if(data_get($item, 'googleID'))
-                                <svg width="13" height="13" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" title="Google account linked">
+                                <svg style="flex-shrink:0;" width="13" height="13" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" title="Google account linked">
                                     <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
                                     <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/>
                                     <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/>
