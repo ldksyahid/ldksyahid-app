907a24b005a4ffa6d67bdc2ea84e1a521a611c85
Farisifakhri <mu.fakhrialfarisi@gmail.com>
2026-06-25 21:06:50 +0700

    fix(seeder): daftarkan CreateUsersSeeder di DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CreateUsersSeeder::class,
             PermissionSeeder::class,
         ]);
     }
